7d2e5a59d73d3b2e9cfcbde8da5a4995525fb1e4
Thiago Bianco <thiago.bianco98@gmail.com>
2022-10-05 21:16:06 -0300

    Update tabuada-subt.py

--- tabuada-subt.py
+++ tabuada-subt.py
@@ -1,6 +1,6 @@
 for tab in range(1, 11):
     print(f'TABUADA DO {tab}:')
-    for c in range(1, 21):
+    for c in range(0, 21):
         resultado_soma = tab + c
         print(f'{resultado_soma:<2} - {tab:>2} = {resultado_soma - tab}')
     print('-' * 14)  
