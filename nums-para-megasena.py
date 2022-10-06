6df66e85beb4a40b6cf7101cd13621f18a80e0f0
Thiago Bianco <thiago.bianco98@gmail.com>
2022-10-06 11:10:55 -0300

    Create nums-para-megasena.py

---
+++ nums-para-megasena.py
@@ -0,0 +1,18 @@
+from random import choice, randint
+
+aposta = []
+total = int(input('Informe o número de apostas desejada: '))
+for i in range(0, total):
+    for c in range(0, 6):
+        while True:
+            n = randint(1, 61)
+            if n not in aposta:
+                aposta.append(n)
+                break
+            else:
+                continue
+    print(f'* {i+1}º JOGO: ', end='')
+    for n in aposta:
+        print(n, end=' ')
+    print()
+    aposta.clear()
